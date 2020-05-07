83daebda170364afd3aac8b256046714016ae24d
Tóth Dániel <63225508+danieltoth02@users.noreply.github.com>
2020-05-07 12:28:09 +0200

    Update beadando32.py

--- beadando32.py
+++ beadando32.py
@@ -1,4 +1,4 @@
 import numpy as np
-lista=["E","X","O"]
-jatek=np.random.choice(lista,(3,3))
+lista=["E","X","O"]                                                       #letrehozzuk a listat melyben a lehetseges karaktereket taroljuk
+jatek=np.random.choice(lista,(3,3))                                       #generalunk egy random vegeredmenyt a lista elemeibol
 print (jatek)
