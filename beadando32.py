3f4a8463e75bfa520e718476c32275f0b261b47b
Tóth Dániel <63225508+danieltoth02@users.noreply.github.com>
2020-05-11 15:15:21 +0200

    Update beadando32.py

--- beadando32.py
+++ beadando32.py
@@ -9,26 +9,26 @@ dontetlen="igen"
 if jatek[0,0]==jatek[0,1]==jatek[0,2] and jatek[0,0]!="E":
     print (jatek[0,0])
     dontetlen = "nem"
-if jatek[1,0]==jatek[1,1]==jatek[1,2] and jatek[1,0]!="E":
+elif jatek[1,0]==jatek[1,1]==jatek[1,2] and jatek[1,0]!="E":
     print (jatek[1,0])
     dontetlen = "nem"
-if jatek[2,0]==jatek[2,1]==jatek[2,2] and jatek[2,0]!="E":
+elif jatek[2,0]==jatek[2,1]==jatek[2,2] and jatek[2,0]!="E":
     print (jatek[2,0])
     dontetlen = "nem"
-if jatek[0,0]==jatek[1,0]==jatek[2,0] and jatek[0,0]!="E":
+elif jatek[0,0]==jatek[1,0]==jatek[2,0] and jatek[0,0]!="E":
     print (jatek[0,0])
     dontetlen = "nem"
-if jatek[0,1]==jatek[1,1]==jatek[2,1] and jatek[0,1]!="E":
+elif jatek[0,1]==jatek[1,1]==jatek[2,1] and jatek[0,1]!="E":
     print (jatek[0,1])
     dontetlen = "nem"
-if jatek[0,2]==jatek[1,2]==jatek[2,2] and jatek[0,2]!="E":
+elif jatek[0,2]==jatek[1,2]==jatek[2,2] and jatek[0,2]!="E":
     print (jatek[0,2])
     dontetlen = "nem"
-if jatek[0,0]==jatek[1,1]==jatek[2,2] and jatek[0,0]!="E":
+elif jatek[0,0]==jatek[1,1]==jatek[2,2] and jatek[0,0]!="E":
     print (jatek[0,0])
     dontetlen = "nem"
-if jatek[0,2]==jatek[1,1]==jatek[2,0] and jatek[0,2]!="E":
+elif jatek[0,2]==jatek[1,1]==jatek[2,0] and jatek[0,2]!="E":
     print (jatek[0,2])
     dontetlen = "nem"
 if dontetlen=="igen":
-    print ("Egyenlő")
+    print ("Egyenlo")
