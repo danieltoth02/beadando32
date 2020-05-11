b3ea9ae34927cc73423a8a49aae2d85ce093e8bc
Tóth Dániel <63225508+danieltoth02@users.noreply.github.com>
2020-05-11 14:40:02 +0200

    Update beadando32.py

--- beadando32.py
+++ beadando32.py
@@ -1,22 +1,34 @@
 import numpy as np
-lista=["E","X","O"]                                                     
-jatek=np.random.choice(lista,(3,3))                                  
+
+lista=["E","X","O"]
+jatek=np.random.choice(lista,(3,3))
 print (jatek)
+
+dontetlen="igen"
+
 if jatek[0,0]==jatek[0,1]==jatek[0,2] and jatek[0,0]!="E":
     print (jatek[0,0])
+    dontetlen = "nem"
 if jatek[1,0]==jatek[1,1]==jatek[1,2] and jatek[1,0]!="E":
     print (jatek[1,0])
+    dontetlen = "nem"
 if jatek[2,0]==jatek[2,1]==jatek[2,2] and jatek[2,0]!="E":
     print (jatek[2,0])
+    dontetlen = "nem"
 if jatek[0,0]==jatek[1,0]==jatek[2,0] and jatek[0,0]!="E":
     print (jatek[0,0])
+    dontetlen = "nem"
 if jatek[0,1]==jatek[1,1]==jatek[2,1] and jatek[0,1]!="E":
     print (jatek[0,1])
+    dontetlen = "nem"
 if jatek[0,2]==jatek[1,2]==jatek[2,2] and jatek[0,2]!="E":
     print (jatek[0,2])
+    dontetlen = "nem"
 if jatek[0,0]==jatek[1,1]==jatek[2,2] and jatek[0,0]!="E":
     print (jatek[0,0])
+    dontetlen = "nem"
 if jatek[0,2]==jatek[1,1]==jatek[2,0] and jatek[0,2]!="E":
     print (jatek[0,2])
-else:
-    print ("egyenlo")
+    dontetlen = "nem"
+if dontetlen=="igen":
+    print ("Egyenlő")
